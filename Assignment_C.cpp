1a4d4f1f2af2d328a1ea9e40507e257ad7ec62f6
markmaithyamumo <markmaithya26@gmail.com>
2026-09-14 18:02:31 +0300

    Add files via upload

---
+++ Assignment_C.cpp
@@ -0,0 +1,16 @@
+/*
+Author:Mark Maithya Mumo
+admission number:BCS-05-0574/2026
+*/
+#include<stdio.h>
+int main(){
+	//ASSIGNMENT
+	printf("Units Offered This semester\n");
+	printf("1. Introduction to programming\n");
+	printf("2. Data Structures\n");
+	printf("3. Computer Networks\n");
+	printf("4. Database Systems\n");
+	printf("5. Software Engineering");
+}
+//Assignment Task 1
+//Displaying the units on offer
